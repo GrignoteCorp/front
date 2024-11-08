7e36e8d17d3ef7d9bcaa642e70339ade527a58dd
Lucie Ménard <lulu.menard@gmail.com>
2024-11-08 11:24:40 +0100

    refacto store vuex

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,14 +1,67 @@
 import { createStore } from 'vuex'
-import state from './state'
-import * as actions from './actions.js'
-import * as mutations from './mutations.js'
-import * as getters from './getters.js'
+import axiosClient from '@/axiosClient'
 
 const store = createStore({
-  state,
-  actions,
-  mutations,
-  getters
+  state: {
+    searchedMeals: [],
+    mealsByLetter: [],
+    mealsByIngredients: [],
+    mealDetails: [],
+    ingredientsList: []
+  },
+  actions: {
+    searchMeals({ commit }, keyword) {
+      axiosClient.get(`search.php?s=${keyword}`).then(({ data }) => {
+        commit('setSearchedMeals', data.meals)
+      })
+    },
+
+    searchMealsByLetter({ commit }, letter) {
+      axiosClient.get(`search.php?f=${letter}`).then(({ data }) => {
+        commit('setMealsByLetter', data.meals)
+      })
+    },
+
+    searchMealsByIngredient({ commit }, ingredient) {
+      axiosClient.get(`filter.php?i=${ingredient}`).then(({ data }) => {
+        commit('setMealsByIngredient', data.meals)
+      })
+    },
+
+    searchMealDetails({ commit }, idMeal) {
+      axiosClient.get(`lookup.php?i=${idMeal}`).then(({ data }) => {
+        commit('setMealDetails', data.meals[0])
+      })
+    },
+
+    searchIngredientsList({ commit }) {
+      axiosClient.get(`list.php?i=list`).then(({ data }) => {
+        commit('setIngredientsList', data.meals)
+      })
+    }
+  },
+  mutations: {
+    setSearchedMeals(state, meals) {
+      state.searchedMeals = meals
+    },
+
+    setMealsByLetter(state, meals) {
+      state.mealsByLetter = meals
+    },
+
+    setMealsByIngredient(state, meals) {
+      state.mealsByIngredient = meals
+    },
+
+    setMealDetails(state, meals) {
+      state.mealDetails = meals
+    },
+
+    setIngredientsList(state, meals) {
+      state.ingredientsList = meals
+    }
+  },
+  getters: {}
 })
 
 export default store
--- src/stores/state.js
+++
@@ -1,7 +0,0 @@
-export default {
-  searchedMeals: [],
-  mealsByLetter: [],
-  mealsByIngredients: [],
-  mealDetails: [],
-  ingredientsList: []
-}
